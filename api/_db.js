08ed7207e582681d7a1c34045a288d2acac42e92
manibabu2006 <komanapallimanibabu2006@gmail.com>
2026-01-28 05:10:08 +0530

    Fix Aiven SSL self-signed certificate issue

--- api/_db.js
+++ api/_db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: true }   // ⭐ REQUIRED for Aiven
+  ssl: { rejectUnauthorized: false }   // ⭐ REQUIRED for Aiven
 });
 
 export default pool;
