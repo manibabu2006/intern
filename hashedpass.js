10617b7eed4f1c7f1f6055cf19d4cc5b8b71424b
manibabu2006 <komanapallimanibabu2006@gmail.com>
2026-01-28 02:38:31 +0530

    Delete hashedpass.js

--- hashedpass.js
+++
@@ -1,4 +0,0 @@
-const bcrypt = require('bcryptjs');
-const password = 'mani123'; // Replace with your desired password
-const hash = bcrypt.hashSync(password, 10);
-console.log('Hashed password:', hash);
